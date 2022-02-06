3d48fd9bfec05f6231fa23fda30a62a8c4a92aee
Stephen Karukas <skarukas@iu.edu>
2022-02-06 14:06:40 -0500

    Add a few experimental results

--- jupyter-notebooks/access_partition.sh
+++ jupyter-notebooks/access_partition.sh
@@ -2,4 +2,5 @@
 
 module unload deeplearning
 
-srun -p dl --gpus-per-node p100:1 --time=03:00:00 --pty bash
+#srun -p dl --gpus-per-node p100:1 --time=03:00:00 --pty bash
+srun -p dl --time=01:00:00 --pty bash
