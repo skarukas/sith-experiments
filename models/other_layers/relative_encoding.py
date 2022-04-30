1ade8429fbd56a05a58ce6da0f2fdc40123e5adf
Stephen Karukas <skarukas@iu.edu>
2022-04-30 13:21:23 -0400

    Add modules for passing relative pose info across layers

---
+++ models/other_layers/relative_encoding.py
@@ -0,0 +1,108 @@
+# relative_encoding.py - PyTorch layers for encoding vectors (x_1, x_2, ..., x_d)
+#   such that f(x_1, x_2, ..., x_d) = f(x_1+k, x_2+k, ..., x_d+k) for all k
+# Author: Stephen Karukas
+
+import torch
+from torch import nn
+from math import pi
+import random
+
+
+class RelativeEncoding(nn.Module):
+    def __init__(self, dim=-1, method="exponential", temperature=1):
+        """
+        Create an encoding of the input vector such that:
+        - f(x_1, x_2, ...) = f(x_1+k, x_2+k, ...) for all k
+        """
+        super().__init__()
+        # method = exponential | linear
+        self.method = method
+        self.dim = dim
+        self.temperature = temperature
+    
+
+    def forward(self, tens):
+        if self.method == "linear":
+            # x_i - mean(x)
+            y = tens - tens.mean(self.dim, keepdim=True)
+        else:
+            # modified softmax: exp(x_i) / (sum(exp(x)) - exp(x_i))
+            xp = torch.exp(self.temperature*tens)
+            sm = xp.sum(self.dim, keepdim=True)
+            y = xp / (sm - xp)
+        return y
+
+
+class PeriodicRelativeEncoding(nn.Module):
+    def __init__(self, period, dim=-1, real=False):
+        """
+        Create an encoding of the input vector such that:
+        - `f(x_1, x_2, ...) = f(x_1+k, x_2+k, ...)` for all `k`
+        - `f(x_1, x_2, ...) = f(x_1+a_1*m, x_2+a_2*m, ...)`
+            for integer `(a_1, a_2, ...)` and period `m`.
+        
+        - If `real` is `False`, the output will be a tensor where 
+          `out.shape[dim] = 2*d`, with the real part of the function 
+          as the first `d` elements and the imaginary part as the 
+          next `d` elements. `d=inp.shape[dim]`
+        - If `real` is `True`, only the real part will be returned.
+        
+        Note: (I think) the complex representation satisfies 
+            `f(x) = f(x') iff x' = x + k + a*m` (integer vector `a`, real scalar `k`),
+          but the real version does not. No smooth real periodic function 
+          is bijective in [0, m) -- one output will map to multiple inputs, so
+          the DNN would have to learn to disambiguate the output.
+        """
+        super().__init__()
+        self.dim = dim
+        self.real = real
+        self.m = period
+    
+
+    def forward(self, tens):
+        # modified complex softmax: cexp(x_i) / (sum(cexp(x)) - cexp(x_i))
+        xp = torch.exp(-2j*pi*tens / self.m)
+        sm = xp.sum(self.dim, keepdim=True)
+        y = xp / (sm - xp)
+        
+        return y.real if self.real else torch.cat((y.real, y.imag), self.dim)
+
+
+if __name__ == "__main__":
+    # test non-periodic layer
+    eps = 1e-4
+    x = torch.randn(10, 5, 6, 15)
+
+    for method in ("linear", "exponential"):
+        for dim in range(len(x.shape)):
+            encoder = RelativeEncoding(dim=dim, method=method)
+            out = encoder(x)
+            assert out.shape == x.shape
+
+            # add random number to all
+            out1 = encoder(x + torch.randn(1))
+            err = ((out1 - out)**2).sum()
+            assert err < eps, err
+
+    # test periodic layer
+    for real in (True, False):
+        for dim in range(len(x.shape)):
+            m = random.randint(5, 20)
+            encoder = PeriodicRelativeEncoding(m, dim=dim, real=real)
+            out = encoder(x)
+            z = 2 - real
+            assert out.shape[dim] == x.shape[dim]*z
+
+            # add random number to all
+            out1 = encoder(x + torch.randn(1))
+            err = ((out1 - out)**2).sum()
+            assert err < eps, err
+
+            # add random multiple of period to each
+            shape = [1] * len(x.shape)
+            shape[dim] = x.shape[dim]
+            shape = tuple(shape)
+            a = torch.randint(0, 10, shape)
+            out2 = encoder(x + a*m)
+            err = ((out2 - out)**2).sum()
+            assert err < eps, err
